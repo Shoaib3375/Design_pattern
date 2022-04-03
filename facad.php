a13d5e4d390fc20f375ee2c7e751533bb23097de
Shoaib3375 <mestabd60@gmail.com>
2022-04-04 04:35:20 +0600

    Aded facad & sigleton

---
+++ facad.php
@@ -0,0 +1,4 @@
+<?php
+require_once('singleton.php');
+$singleton = new Singleton();
+$anotherObject = new Singleton();
---
+++ singleton.php
@@ -0,0 +1,8 @@
+<?php
+class singleton{
+    private static $instance ;
+    private function __construct(){}
+    public function getInstance(){
+
+    }
+}
